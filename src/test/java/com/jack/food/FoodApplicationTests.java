abe66c4fbbbc659d04ccb3340d72431859a88cf5
Jack Ziegler <64660004+wumby@users.noreply.github.com>
2023-11-09 13:13:02 -0700

    init

---
+++ src/test/java/com/jack/food/FoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jack.food;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
